bb563f01afe3666d11dbd306c0de0b137edb805e
allison ruiz <allisonruiz@allisons-MacBook-Pro.local>
2022-06-19 18:16:19 -0600

    places.js

--- controllers/places.js
+++ controllers/places.js
@@ -1,10 +1,80 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
+//INDEX
+router.get('/', (req, res) => {
+  db.Place.find()
+  .then((places) => {
+    res.render('places/index', {places})
+  })
+  .catch(err => {
+    console.log(err)
+    res.render('error404')
+  })
+})
+
+//create
+router.post('/', (req, res) => {
+  db.Place.create(req.body)
+  .then(() => {
+    res.redirect('/places')
+  })
+  .catch(err => {
+    if (err && err.name == 'ValidationError'){
+      let message = 'Validation Error:'
+      for (var field in err.errors){
+        message += `${field} was ${err.errors[field].value}.`
+        message += `${err.errors[field].message}`
+      }
+      res.render('places/new', {message})
+    }
+    else {
+      res.render('error404')
+    }
+  })
+})
+
+
+//New
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
+//show
+router.get('/:id', (req, res) => {
+  db.Place.findById(req.params.id)
+  .populate('comments')
+  .then(place => {
+    console.log(place.comments)
+    res.render('places/show', {place})
+  })
+  .catch(err => {
+    console.log('err', err)
+    res.render('error404')
+  })
+})
 
+//update
+router.put('/:id', (req, res) => {
+  db.Place.findByIdAndUpdate(req.params.id,req.body)
+  .then(() => {
+    res.redirect(`/places/${req.params.id}`)
+  })
+  .catch(err => {
+    console.log('err', err)
+    res.render('error404')
+  })
+})
+
+//delete
+router.delete('/:id', (req, res) => {
+  db.Place.findByIdAndDelete(req.params.id)
+  .then(place => {
+    res.redirect('/places')
+  })
+  .catch(err => {
+    res.render('error404')
+  })
+})
   router.post('/', (req, res) => {
     console.log(req.body)
     if (!req.body.pic) {
@@ -23,20 +93,6 @@ router.get('/new', (req, res) => {
   
   
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
-      
     res.render('places/index', {places})
 })
 
